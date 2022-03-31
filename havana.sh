904415a0c61e45a5706d2b1a1263043f3a357c1e
janniswarnat <jannis.warnat@fit.fraunhofer.de>
2022-03-31 15:14:25 +0200

    both docker-desktop and havana working

--- havana.sh
+++ havana.sh
@@ -2,13 +2,13 @@
 
 set -ex
 
-kubectl config use-context docker-desktop
+kubectl config use-context havana
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.2/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f ingress-nginx-controller-deployment.yaml
+#kubectl apply -f ingress-nginx-controller-deployment.yaml
 #k3d cluster delete k3s-default
 #k3d cluster create --k3s-arg "--disable=traefik@server:*" --volume "$(pwd)/helm-ingress-nginx.yaml:/var/lib/rancher/k3s/server/manifests/helm-ingress-nginx.yaml" --api-port 6550 -p "8000:30000@loadbalancer" -p "9000:31000@loadbalancer" -p "3000:32000@loadbalancer" -p "4505:30001@loadbalancer" -p "4506:30002@loadbalancer" -p "80:80@loadbalancer" -p "443:443@loadbalancer" --agents 2
 # ConfigMaps
-kubectl apply -f tcp-services-configmap.yaml,dynflow-yml-configmap.yaml,foreman-conf-configmap.yaml,foreman-yaml-configmap.yaml,master-pem-configmap.yaml,master-pub-configmap.yaml,minion-configmap.yaml,minion-pem-configmap.yaml,minion-pub-configmap.yaml,raspberrypi-pub-configmap.yaml,linksmart-black-iot-pub-configmap.yaml,pillar-default-configmap.yaml,pillar-raspberrypi-configmap.yaml,pillar-linksmart-configmap.yaml,pillar-top-configmap.yaml,salt-files-configmap.yaml,salt-yml-configmap.yaml,settings-yml-configmap.yaml,supervisor-conf-configmap.yaml
+kubectl apply -f dynflow-yml-configmap.yaml,foreman-conf-configmap.yaml,foreman-yaml-configmap.yaml,master-pem-configmap.yaml,master-pub-configmap.yaml,minion-configmap.yaml,minion-pem-configmap.yaml,minion-pub-configmap.yaml,raspberrypi-pub-configmap.yaml,linksmart-black-iot-pub-configmap.yaml,pillar-default-configmap.yaml,pillar-raspberrypi-configmap.yaml,pillar-linksmart-configmap.yaml,pillar-top-configmap.yaml,salt-files-configmap.yaml,salt-yml-configmap.yaml,settings-yml-configmap.yaml,supervisor-conf-configmap.yaml
 # Services Foreman
 kubectl apply -f redis-cache-service.yaml,redis-tasks-service.yaml,db-service.yaml,app-service.yaml
 # Services Salt etc.
@@ -29,8 +29,8 @@ kubectl wait --for=condition=available --timeout=300s deployment/salt
 # Deploy Foreman
 kubectl apply -f app-deployment.yaml,orchestrator-deployment.yaml,worker-deployment.yaml
 kubectl wait --for=condition=available --timeout=300s deployment/app
-kubectl wait --for=condition=available --timeout=300s deployment/ingress-nginx-controller -n ingress-nginx
-kubectl apply -f ingress-local.yaml
+#kubectl wait --for=condition=available --timeout=300s deployment/ingress-nginx-controller -n ingress-nginx
+kubectl apply -f ingress.yaml
 
 #kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.1.2/deploy/static/provider/cloud/deploy.yaml
 
